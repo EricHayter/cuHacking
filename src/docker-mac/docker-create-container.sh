04463d313ad688e56f0065b585d2146d086c0250
taisirhassan <taisirisawesome@gmail.com>
2025-03-15 11:20:54 -0400

    Enhance Docker setup by adding project root volume and updating Makefile paths for container execution

--- src/docker-mac/docker-create-container.sh
+++ src/docker-mac/docker-create-container.sh
@@ -2,11 +2,15 @@
 
 QNX_SDP_VERSION=qnx800
 
+# Get the project root directory (2 levels up from this script)
+PROJECT_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
+
 docker create -it --name=qnx-build \
   --platform linux/amd64 \
   --net=host \
   --privileged \
   -v $HOME:/home/$USER/ \
+  -v $PROJECT_ROOT:/workspace \
   "$QNX_SDP_VERSION:latest" /bin/bash --rcfile /usr/local/qnx/.qnxbashrc
 
 docker start qnx-build
