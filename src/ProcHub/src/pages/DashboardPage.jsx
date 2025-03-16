5ab8c420d22e126cc0cf35f996d11f97f885e603
Eric <hayter.eric@gmail.com>
2025-03-15 23:40:05 -0400

    Adding websocket starting support

--- src/ProcHub/src/pages/DashboardPage.jsx
+++ src/ProcHub/src/pages/DashboardPage.jsx
@@ -1,9 +1,89 @@
-import React, { useState } from 'react';
-import Navbar from '../components/Navbar';
-import ProcessTable from '../components/ProcessTable';
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import ProcessTable from "../components/ProcessTable";
 
 const DashboardPage = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(true);
+  const [data, setData] = useState(null);
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://172.20.10.10");
+
+    ws.onmessage = (event) => {
+      try {
+        const receivedData = JSON.parse(event.data);
+		console.log(receivedData);
+        switch (receivedData["request_type"]) {
+          case "GetProcesses":
+            for (let i = 0; i < receivedData["pids"].length; i++) {
+              getSimpleProcessDetails(ws, receivedData["pids"][i]);
+            }
+			// copy it over to a new map
+            break;
+        }
+        // setData(receivedData);
+      } catch (error) {
+        console.error("Invalid JSON received:", error);
+      }
+    };
+
+    ws.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    ws.onclose = () => {
+      console.log("WebSocket connection closed");
+    };
+
+    setSocket(ws);
+
+    const interval = setInterval(() => {
+      getProcesses(ws);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+      ws.close();
+    };
+  }, []);
+
+  const getProcesses = (ws) => {
+    if (ws.readyState === WebSocket.OPEN) {
+      const message = JSON.stringify({ request_type: "GetProcesses" });
+      ws.send(message);
+    }
+  };
+
+  const getSimpleProcessDetails = (ws, pid) => {
+    if (ws.readyState === WebSocket.OPEN) {
+      const message = JSON.stringify({
+        request_type: "GetSimpleProcessDetalis",
+        pid: pid,
+      });
+      ws.send(message);
+    }
+  };
+
+  const getDetailedProcessDetails = (ws, pid) => {
+    if (ws.readyState === WebSocket.OPEN) {
+      const message = JSON.stringify({
+        request_type: "GetDetailedProcessDetails",
+        pid: pid,
+      });
+      ws.send(message);
+    }
+  };
+
+  const suspendProcess = (ws, pid) => {
+    if (ws.readyState === WebSocket.OPEN) {
+      const message = JSON.stringify({
+        request_type: "SuspendProcess",
+        pid: pid,
+      });
+      ws.send(message);
+    }
+  };
 
   const handleMenuClick = () => {
     setSidebarOpen(!isSidebarOpen);
@@ -12,10 +92,9 @@ const DashboardPage = () => {
   return (
     <div>
       <Navbar onMenuClick={handleMenuClick} />
-      <ProcessTable />
+      <ProcessTable data={data}/>
     </div>
   );
 };
 
 export default DashboardPage;
-
