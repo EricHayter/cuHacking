5fb0bcf21b25c446a6da5e4a905a0d2e4a11665f
Eric <hayter.eric@gmail.com>
2025-04-13 18:47:49 -0400

    Moved QNX RPM server code to new repository

--- src/server/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- 
-source /usr/local/qnx/env/bin/activate
-source $HOME/qnx800/qnxsdp-env.sh
- 
-cd $HOME/qnxprojects/cuHacking/src/server/
- 
-ntoaarch64-gcc -o proc-monitor -I. proc-monitor.c proc_core.c proc_group.c proc_history.c socket_server.c
